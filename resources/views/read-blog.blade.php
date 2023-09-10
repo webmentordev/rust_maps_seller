12ab9e8fcbf43d682f69417083a3e21334348e7c
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-09-10 18:58:04 +0500

    Reponsive design fix for blog

--- resources/views/read-blog.blade.php
+++ resources/views/read-blog.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section class="w-full">
         <div class="max-w-5xl m-auto pt-[150px] pb-12 px-4">
-            <div class="border border-white/10 p-12 rounded-lg bg-dark-100">
+            <div class="border border-white/10 p-12 690px:p-0 690px:bg-transparent 690px:border-none rounded-lg bg-dark-100">
                 <h1 class="blog-title text-3xl mb-2 text-white">{{ $blog->title }}</h1>
                 <ul class="flex flex-col mb-3 text-rust">
                     <span class="blog-title">Posted: 
@@ -15,7 +15,7 @@
                     @endif
                 </ul>
                 <main class=" py-2 blog">
-                    <img src="{{ asset('/storage/'.$blog->thumbnail) }}" alt="{{ $blog->title }} Image">
+                    <img src="{{ asset('/storage/'.$blog->thumbnail) }}" title="{{ $blog->title }}" alt="{{ $blog->title }} Image">
                     {!! $blog->body !!}
                 </main>
             </div>
